46381b2ea4ff44e5fdc34c0ee64430dad73ca3fb
Kellie Kim <kellie.kim1@gmail.com>
2018-03-30 00:38:09 -0700

    Add grab_Alu.py to parse Alu sequences from Alu DB

---
+++ grab_Alu.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+with open("Alu_sublibrary","U") as input_file:
+	tokens = input_file.readlines()
+	start = False
+	cur_seq = ""
+	cur_alu = ""
+	with open("Alu_seqs.tsv","w") as output_file:
+		for index,line in enumerate(tokens):
+			# Found an Alu element to start
+			if line.startswith(";ORIGIN"):
+				print "found an alu"
+				start = True
+				cur_alu = "> " + tokens[index+1] # Add next line as index
+				cur_alu_mark = tokens[index+1]
+			elif start == False:
+				pass
+			elif line.startswith(cur_alu_mark):
+				print "skipping header"
+				pass
+			elif line.startswith(";LOCUS"):
+				print "writing to file"
+				output_file.write(cur_alu + cur_seq + "\n")
+				start = False
+				cur_seq = ""
+			elif start == True:
+				# Add sequence
+				cur_seq += line.strip()
