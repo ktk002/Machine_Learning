8544bedaefb20cdcf71450d92e880cd0aafe9499
Kellie Kim <kellie.kim1@gmail.com>
2018-09-15 21:32:58 -0700

    Add function to convert DNA to list between 0-3
    - Added function dna_to_number to return list with values between 0-3
    - Added function to load training data into x_train_list attribute

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,37 +3,57 @@
 import tensorflow as tf
 import numpy as np
 
-training_data_types = ['intron', 'alu']
 
 class NeuralNetwork(object):
+    training_data_types = ['intron', 'alu']
+    NEGATIVE_CLASSIFICATION_LABEL = 0
+    POSITIVE_CLASSIFICATION_LABEL_COUNTER = 0
+
     def __init__(self):
         self.x_train_list = [] # List of lists of ATCG values converted to 0-3 (ex: [[0, 1, 2, 3], [1, 1, 1, 1]])
-        self.x_train_normalized = DNA_to_ # List of lists containing normalized ATCG values (ex: [[0, 0.3333, 1],[1, 1 ,1]])
+        self.x_train_normalized = [] # Numpy list of lists containing normalized ATCG values (ex: [[0, 0.3333, 1]])
         self.y_train = [] # List of labels for every sample in x_train_list
         self.x_test = []
         self.y_test = []
-        self.build_nn()
+        self.classification_labels = [] # Labels ranging from 0 to number of classifications - 1
 
     @staticmethod
-    def DNA_to_number(DNA):
+    def dna_to_number(dna):
         """Convert DNA string to list of numbers ranging from 0-3."""
-        return list(DNA.translate(str.maketrans("ATCG", "0123")))
+        return list(dna.translate(str.maketrans("ATCG", "0123")))
 
-    def load_training_data(self, input_file, type="positive"):
-        """Loads data from input_file and adds as positive or negative data."""
+    def load_training_data(self, input_file, type="positive", classification="binary"):
+        """Loads data from input_file and adds as positive or negative data assuming binary classification."""
+        # Increment to next largest classification label if classification is multi
+        if classification == "multi":
+            NeuralNetwork.POSITIVE_CLASSIFICATION_LABEL_COUNTER = NeuralNetwork.POSITIVE_CLASSIFICATION_LABEL_COUNTER + 1
+
+        # Read in training data file
         with open(input_file, 'r') as input_file_reader:
             for line in input_file_reader:
-                tokens = line.strip().split()
-                identifier = tokens[0]
-                sequence = tokens[1]
+                tokens = line.strip().split("\t")  # Split on tab, sequence is in second column
+                sequence = tokens[1]  # Training sequence to be added to list
+                # Add sequence to training data, regardless of label
+                self.x_train_list.append(NeuralNetwork.dna_to_number(sequence))  # Add list containing numbers from 0-3
 
-                # Add sequence depending on label type and add label
-                if type == "positive":
-                    self.x_train_list.append(DNA_to_number(sequence))
-                    self.y
+                # Negative data will be loaded the same for binary and multiclass classification
+                if type == "negative":
+                    self.y_train.append(NeuralNetwork.NEGATIVE_CLASSIFICATION_LABEL)  # Add label negative training data
+                # Classification is multiclass, label of 0 is non-human dna, non zero is human dna
+                elif type == "positive":
+                    if classification == "binary":
+                        self.y_train.append(1) # Add 1 to y_train
+                    elif classification == "multi":
+                        # Add training data to negative list
+                        self.y_train.append(NeuralNetwork.POSITIVE_CLASSIFICATION_LABEL_COUNTER)
+                    else:
+                        print("Classification type invalid.")
+                else:
+                    print("Training data type invalid.")
 
     def build_nn(self, num_hidden_layers=1, num_hidden_neurons=267, activation="relu", classification="binary"):
         pass
+
 
 def main():
     ##################################
@@ -42,12 +62,12 @@ def main():
     # Loop over each type of positive training data set
 
     # Construct neural networks for each type of training data
-    for training_index, training_data_type in enumerate(training_data_types):
+    for training_index, training_data_type in enumerate(NeuralNetwork.training_data_types):
         # Initialize new neural network
         model1 = NeuralNetwork()
         # First load positive and negative training data into neural network
-        model1.load_training_data(input_file=, type="positive")
-        model1.load_training_data(input_file=e, type="negative")
+        model1.load_training_data(input_file="all_alu.tsv", type="positive")
+#        model1.load_training_data_binary(input_file="", type="negative")
 
         # Vary number of hidden layers
         # 1) 1 hidden layer, 267 hidden neurons
@@ -86,7 +106,7 @@ def main():
         # Vary activation function
         # 1) 1 hidden layer, 267 hidden neurons, rectified linear unit activation function (relu)
         model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="relu")
-        # 2) 1 hidden layer, 267 hidden neurons, scaled exponential linear activation function (selu) --> self normalizing
+        # 2) 1 hidden layer, 267 hidden neurons, scaled exponential linear activation function (selu)-self normalizing
         model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="selu")
         # 3) 1 hidden layer, 267 hidden neurons, hyperbolic tangent activation function (tanh)
         model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="tanh")
@@ -98,6 +118,7 @@ def main():
     ##################################
     ### Multiclass classification ####
     ##################################
+
 
 if __name__ == '__main__':
     main()
