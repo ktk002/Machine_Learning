a0dda2900a487ac2b72bc6ecb8e2d87bf9fd32a7
Kellie Kim <kellie.kim1@gmail.com>
2018-09-17 03:07:37 -0700

    Add compile, fit, functions and streamlined NN cases.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env/ python
 
+__author__ = 'Kellie Kim'
+
 import tensorflow as tf
 import numpy as np
 
@@ -10,17 +12,27 @@ class NeuralNetwork(object):
     POSITIVE_CLASSIFICATION_LABEL_COUNTER = 0
 
     def __init__(self):
-        self.x_train_list = [] # List of lists of ATCG values converted to 0-3 (ex: [[0, 1, 2, 3], [1, 1, 1, 1]])
-        self.x_train_normalized = [] # Numpy list of lists containing normalized ATCG values (ex: [[0, 0.3333, 1]])
-        self.y_train = [] # List of labels for every sample in x_train_list
+        self.x_train_list = []  # List of lists of ATCG values converted to 0-3 (ex: [[0, 1, 2, 3], [1, 1, 1, 1]])
+ #       self.x_train_normalized = []  # Numpy list of lists containing normalized ATCG values (ex: [[0, 0.3333, 1]])
+        self.y_train = []  # List of labels for every sample in x_train_list
         self.x_test = []
         self.y_test = []
-        self.classification_labels = [] # Labels ranging from 0 to number of classifications - 1
+        self.model = tf.keras.Sequential()
+        self.predictions = []  # Classification predictions for unknown test data
 
     @staticmethod
     def dna_to_number(dna):
         """Convert DNA string to list of numbers ranging from 0-3."""
         return list(dna.translate(str.maketrans("ATCG", "0123")))
+
+    def normalize_dna(self):
+        """Takes x_train_list attribute and normalizes values in between 0 and 1."""
+        self.x_train_list = tf.keras.utils.normalize(np.array(self.x_train_list), axis=1)
+
+    def convert_to_numpy(self):
+        """Converts training data and labels to numpy arrays."""
+        self.x_train_list = np.array(self.x_train_list)
+        self.y_train = np.array(self.y_train)
 
     def load_training_data(self, input_file, type="positive", classification="binary"):
         """Loads data from input_file and adds as positive or negative data assuming binary classification."""
@@ -51,8 +63,60 @@ class NeuralNetwork(object):
                 else:
                     print("Training data type invalid.")
 
+    def add_input_layer(self, num_features=100, num_neurons=267):
+        """Add first layer of neural network and explicitly specify input shape of data."""
+        self.model.add(tf.keras.layers.Dense(num_hidden_neurons=num_neurons, input_shape=(num_features,)))  # Add first input layer to Sequential model object
+
+    def add_layers(self, num_layers=1, num_neurons=267, activation_function=tf.nn.relu):
+        """Adds {num_layers} to model with {num_neurons} in each hidden layer and {activation_function}."""
+        for index in range(num_layers):
+            self.model.add(tf.keras.layers.Dense(num_neurons, activation=activation_function))
+
+    def add_output_layer(self, activation_function=tf.nn.softmax, classification="binary"):
+        """Adds final layer of neural network using number of classification labels."""
+        if classification == "binary":
+            num_nodes = 1  # Only 1 output required to make binary decision
+            self.model.add(tf.keras.layers.Dense(num_nodes, activation=activation_function))
+        else:
+            #  Multiclassification, add nodes equal to number of classifications
+            num_nodes = NeuralNetwork.POSITIVE_CLASSIFICATION_LABEL_COUNTER
+            self.model.add(tf.keras.layers.Dense(num_nodes, activation=activation_function))
+
+    def compile_model(self, optimizer='adam', loss='binary_crossentropy'):
+        """Adds optimizer and loss function of neural network."""
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+    def fit_model(self, epochs=3):
+        """Trains neural network model using {epochs} number of epochs."""
+        self.model.fit(self.x_train_list, self.y_train, epochs=epochs)
+
+    def print_validation_loss_accuracy(self):
+        """Prints validation loss and validation accuracy from the neural network model with test data sets as input."""
+        val_loss, val_acc = self.model.evaluate(self.x_test, self.y_test)
+        print("validation loss is: ", val_loss)
+        print("validation accuracy is: ", val_acc)
+
+    def predict_unknown_datasets(self):
+        """Use trained model to apply classification labels to unknown test data."""
+        self.predictions = self.model.predict([self.x_test])
+
+    def convert_classification_labels(self):
+        """Takes argmax of each prediction to obtain final list of classification labels."""
+        new_predictions = []
+        for sample_number in range(self.predictions):
+            new_predictions.append(np.argmax(self.predictions[sample_number]))
+        self.predictions = new_predictions
+
     def build_nn(self, num_hidden_layers=1, num_hidden_neurons=267, activation="relu", classification="binary"):
-        pass
+        # Convert numerical values between 0 and 1 instead of 0-3
+        self.normalize_dna()
+        # After loading training data and normalizing, convert training data and labels to numpy arrays
+        self.convert_to_numpy()
+        self.add_input_layer()
+        self.add_layers()
+        self.add_output_layer()
+        self.compile_model()
+        self.fit_model()
 
 
 def main():
@@ -60,60 +124,48 @@ def main():
     ###### Binary classification #####
     ##################################
     # Loop over each type of positive training data set
-
     # Construct neural networks for each type of training data
     for training_index, training_data_type in enumerate(NeuralNetwork.training_data_types):
         # Initialize new neural network
         model1 = NeuralNetwork()
         # First load positive and negative training data into neural network
         model1.load_training_data(input_file="all_alu.tsv", type="positive")
-#        model1.load_training_data_binary(input_file="", type="negative")
+        model1.load_training_data(input_file="", type="negative")
 
-        # Vary number of hidden layers
+        # Vary number of hidden layers cases:
         # 1) 1 hidden layer, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267)
         # 2) 2 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=2, num_hidden_neurons=267)
         # 3) 3 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=3, num_hidden_neurons=267)
         # 4) 4 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=4, num_hidden_neurons=267)
         # 5) 5 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=5, num_hidden_neurons=267)
         # 6) 10 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=10, num_hidden_neurons=267)
         # 7) 25 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=25, num_hidden_neurons=267)
         # 8) 50 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=50, num_hidden_neurons=267)
         # 9) 75 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=75, num_hidden_neurons=267)
         # 10) 100 hidden layers, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=100, num_hidden_neurons=267)
+        num_hidden_layers_list = [1, 2, 3, 4, 5, 10, 25, 50, 75, 100]
+        for hidden_layer_num in num_hidden_layers_list:
+            model1.build_nn(num_hidden_layers=hidden_layer_num, num_hidden_neurons=267)
 
-        # Vary number of hidden neurons
+        # Vary number of hidden neurons cases:
         # 1) 1 hidden layer, 200 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=200)
         # 2) 1 hidden layer, 267 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267)
         # 3) 1 hidden layer, 300 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=300)
         # 4) 1 hidden layer, 400 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=400)
         # 5) 1 hidden layer, 500 hidden neurons
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=500)
+        num_hidden_neurons_list = [200, 267, 300, 400, 500]
+        for hidden_neuron_num in num_hidden_neurons_list:
+            model1.build_nn(num_hidden_layers=1, num_hidden_neurons=hidden_neuron_num)
 
         # Vary activation function
         # 1) 1 hidden layer, 267 hidden neurons, rectified linear unit activation function (relu)
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="relu")
         # 2) 1 hidden layer, 267 hidden neurons, scaled exponential linear activation function (selu)-self normalizing
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="selu")
         # 3) 1 hidden layer, 267 hidden neurons, hyperbolic tangent activation function (tanh)
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="tanh")
         # 4) 1 hidden layer, 267 hidden neurons, sigmoid activation function (sigmoid)
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="sigmoid")
         # 5) 1 hidden layer, 267 hidden neurons, linear activation function (linear)
-        model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation="linear")
+        activation_type_list = ["relu", "selu", "tanh", "sigmoid", "linear"]
+        for activation_type in activation_type_list:
+            model1.build_nn(num_hidden_layers=1, num_hidden_neurons=267, activation=activation_type)
 
     ##################################
     ### Multiclass classification ####
